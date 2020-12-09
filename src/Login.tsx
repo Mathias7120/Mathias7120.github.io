78d7a7094d12f91797b0762a232fcfddbd53a135
Mathias7120 <42300995+Mathias7120@users.noreply.github.com>
2020-12-09 10:10:12 +0100

    All the things

---
+++ src/Login.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+class Login extends React.Component<{}> {
+  constructor(props: any) {
+    super(props);
+  }
+
+  public render() {
+    return (
+        <h1>Login</h1>
+    );
+  }
+}
+
+export default Login;
---
+++ src/Register.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+class Register extends React.Component<{}> {
+  constructor(props: any) {
+    super(props);
+  }
+
+  public render() {
+    return (
+      <h1>Register</h1>
+    );
+  }
+}
+
+export default Register;
